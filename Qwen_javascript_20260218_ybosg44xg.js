c8f34d314781dbc9b34ed4e1269964cd412b2748
izc05 <isiciozafracantos@gmail.com>
2026-02-18 17:52:16 +0100

    Delete Qwen_javascript_20260218_ybosg44xg.js

--- Qwen_javascript_20260218_ybosg44xg.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = 'isivoltpro-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request) || fetch(e.request));
-});
